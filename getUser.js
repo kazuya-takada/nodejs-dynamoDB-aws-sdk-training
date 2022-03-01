46cdb5d00b0819cf7978233725cfb6ca8a0f774c
kazuya-takada <takada.chopin.work@gmail.com>
2022-03-01 20:33:59 +0900

    feat: ユーザーを1件取得する処理を実装

---
+++ getUser.js
@@ -0,0 +1,16 @@
+const AWS = require('aws-sdk')
+AWS.config.loadFromPath('./config.json')
+const documentClient = new AWS.DynamoDB.DocumentClient()
+
+const params = {
+  TableName: 'users',
+  Key: {
+    'user_id': 1,
+    'created_at': '1544752292'
+  }
+}
+
+documentClient.get(params, (err, data) => {
+  if (err) console.log(JSON.stringify(err, null, 2))
+  else console.log(JSON.stringify(data, null, 2))
+})
